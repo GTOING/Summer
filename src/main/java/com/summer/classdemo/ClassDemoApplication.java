4c376c84e03cce9958298e81875280ab1b733f13
GTOING <1667399679@qq.com>
2023-07-03 10:57:30 +0800

    变更

--- src/main/java/com/summer/classdemo/ClassDemoApplication.java
+++ src/main/java/com/summer/classdemo/ClassDemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ClassDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClassDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ClassDemoApplication.class, args);
+    }
 
 }
