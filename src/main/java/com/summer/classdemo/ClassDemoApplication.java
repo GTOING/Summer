0fd407cdcb36e19acf8fee52b5afadc3513aa58d
GTOING <1667399679@qq.com>
2023-07-03 09:16:55 +0800

    Initial commit

---
+++ src/main/java/com/summer/classdemo/ClassDemoApplication.java
@@ -0,0 +1,13 @@
+package com.summer.classdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassDemoApplication.class, args);
+	}
+
+}
